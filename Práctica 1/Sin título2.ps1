d138ab7b75d0aae36675af4c58435fa0d350e5e9
MiguelPeSe <91191484+MiguelPeSe@users.noreply.github.com>
2021-09-23 10:31:06 +0200

    Add files via upload

---
+++ Práctica 1/Sin título2.ps1
@@ -0,0 +1,27 @@
+﻿write-host "----------------------------"
+write-host "|                          |"
+write-host "|                          |"
+write-host "|                          |"
+write-host "|           MENU           |"
+write-host "|                          |"
+write-host "|                          |"
+write-host "|                          |"
+write-host "----------------------------"
+
+write-host " 1. Convierte de binario a decimal"
+write-host " 2. Convierte de hexadecimal a binario"
+$opción = read-host "selecciona operacion "
+if ($opción -eq 1)
+{
+    $valor = Read-Host "dime el numero binario... "
+    $resultado = [convert]::toint32($valor,2)
+    "el numero es..." + $resultado
+}
+
+$opción = read-host "selecciona operacion "
+if ($opción -eq 2)
+{
+    $valor = Read-Host "dime el numero hexadecimal... "
+    $resultado = [convert]::ToString($valor,2)
+    "el numero es..." + $resultado
+}
